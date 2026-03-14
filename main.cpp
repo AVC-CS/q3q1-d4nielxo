cde62a82d3d981d2d9533f790367c48dc806989b
Daniel <dhaddad3@students.avc.edu>
2026-03-13 19:53:04 -0700

    commit 1

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
-int main()
-{
+int main() {
     double a, b, c;
-    // TODO
+    
+    cin >> a >> b >> c;
 
-    // TODO
-    // TODO
-    // TODO
+    double root1 = (-b + sqrt(b * b - 4 * a * c)) / 2 * a;
+    double root2 = (-b - sqrt(b * b - 4 * a * c)) / 2 * a;
 
-    // TODO
-    // TODO
-
-    cout << setprecision(2) << fixed;
-    // TODO
+    cout << fixed << setprecision(2);
+    
+    if (root1 > root2) {
+        cout << root1 << " " << root2 << endl;
+    } else {
+        cout << root2 << " " << root1 << endl;
+    }
 
     return 0;
 }
